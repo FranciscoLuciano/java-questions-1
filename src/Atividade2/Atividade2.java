26c4fec5c62a589a5a05fc4658a06b490d64eab6
FranciscoLuciano <160194754+FranciscoLuciano@users.noreply.github.com>
2024-07-16 21:42:20 -0300

    Change method Scanner
    Method sc.next() to sc.nextLine()

--- src/Atividade2/Atividade2.java
+++ src/Atividade2/Atividade2.java
@@ -5,16 +5,17 @@ public class Atividade2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String[] dados, dadosDoProfessor = new String[11];
-        dados = new String[]{"Nome ", "Endereço ", "cidade ", "UF ", "CEP ", "telefone ", "CPF ", "RG ", "DatadeNascimento ", "GrauEscola ", "Curso "};
+        String[] dados, dadosDoProfessor;
+        dados = new String[]{"Nome", "Endereço", "Cidade", "UF", "CEP", "Telefone", "CPF", "RG", "Data de Nascimento", "Grau Escolar", "Curso que leciona"};
+        dadosDoProfessor = new String[dados.length];
 
-        for(int i = 0; i<dados.length; i++) {
-            System.out.println(dados[i]+ ":");
-            dadosDoProfessor[i] = sc.next();
+        for(int i = 0; i < dados.length; i++) {
+            System.out.print(dados[i] + ": ");
+            dadosDoProfessor[i] = sc.nextLine();
         }
 
         for(int i=0; i < dados.length; i++){
-            System.out.println(dados[i] + " " + dadosDoProfessor[i]);
+            System.out.println(dados[i] + ": " + dadosDoProfessor[i]);
         }
 
 
